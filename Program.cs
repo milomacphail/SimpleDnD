c59d730ec60ad8dff205a5eb2a696550ad22b397
milomacphail <milomacphail@gmail.com>
2019-08-20 19:08:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpleDnD
+{
+    class Dice
+    {
+        Random rand = new Random();
+        int value;
+
+        public Dice()
+        {
+            this.value = this.RollDice();
+        }
+
+        public int RollDice()
+        {
+            int dieResult = rand.Next(1, 20);
+
+            if (dieResult == 1)
+            {
+                Console.WriteLine("A {0}! Critical miss!", dieResult);
+            }
+            else if (dieResult == 20)
+            {
+                Console.WriteLine("A Critical Hit!");
+            }
+
+            return dieResult;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice gameRoll = new Dice();
+            Console.WriteLine(gameRoll.RollDice());
+        }
+    }
+}
