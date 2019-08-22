7cf8d8ae99b950e747f37d5309449dd67bb9e93a
milomacphail <milomacphail@gmail.com>
2019-08-21 20:38:35 -0400

    Version 1.4 completed - added damage dice class.

--- Program.cs
+++ Program.cs
@@ -30,13 +30,87 @@ namespace SimpleDnD
 
     }
 
-
-    class Program
+    class Modifier
     {
-        static void Main(string[] args)
+        private int _modifierValue;
+        public Modifier()
         {
-            Dice gameRoll = new Dice();
-            Console.WriteLine(gameRoll.RollDice());
+            this._modifierValue = this.EnterModifier();
+        }
+
+        public int EnterModifier()
+        {
+            Console.WriteLine("Dungeon Master, enter a modifier: ");
+            int modifier = Convert.ToInt32(Console.ReadLine());
+            return modifier;
+        }
+
+        class ArmorClass
+        {
+            private int _armorValue;
+
+            public ArmorClass()
+            {
+                this._armorValue = this.EnterArmorClass();
+            }
+
+            public int EnterArmorClass()
+            {
+                Console.WriteLine("Dungeon Master, enter enemy armor class: ");
+                int ac = Convert.ToInt32(Console.ReadLine());
+                return ac;
+            }
+
+        }
+
+        class Hit
+        {
+            public bool IsHit()
+            {
+                bool isHit;
+                Dice gameRoll = new Dice();
+                Modifier gameModifier = new Modifier();
+                int playerRoll = (gameRoll.RollDice() + gameModifier.EnterModifier());
+                ArmorClass enemyArmor = new ArmorClass();
+                int enemyGameArmor = enemyArmor.EnterArmorClass();
+
+                if (enemyGameArmor > playerRoll)
+                {
+                    isHit = true;
+                    Console.WriteLine("You hit the enemy!");
+                }
+                else
+                {
+                    isHit = false;
+                    Console.WriteLine("You missed! Great effort.");
+                }
+
+                return isHit;
+            }
+        }
+
+        class DamageDice
+        {
+            int numberOfDice;
+            int sidedDice;
+            public void DamageToEnemy()
+            {
+                Console.WriteLine("What dice are you using: ");
+                string diceClass = Console.ReadLine();
+                numberOfDice = Int32.Parse(diceClass.Substring(0, 1));
+                sidedDice = Int32.Parse(diceClass.Substring(2, 1));
+            }
+
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                DamageDice rollDamage = new DamageDice();
+                rollDamage.DamageToEnemy();
+            }
         }
     }
 }
